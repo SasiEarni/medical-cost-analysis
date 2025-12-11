af253924a15f69b3e90eaf2095029d10e530e427
Prasannapujaari <pp24acs@herts.ac.uk>
2025-12-11 12:27:28 +0000

    Add files via upload
    Added correlation analysis for medical cost dataset

--- insurance_correlation_study.R
+++ insurance_correlation_study.R
@@ -55,3 +55,21 @@ ds225_no_charges_outliers <- ds225[!ds225$charges %in% charges_outliers, ]
 summary(ds225$bmi)
 summary(ds225$charges)
 summary(ds225_no_charges_outliers$charges)
+
+########################################################
+# 3. CORRELATION ANALYSIS
+########################################################
+
+# Convert categorical variables for correlation
+ds225$smoker_num <- ifelse(ds225$smoker == "yes", 1, 0)
+ds225$sex_num <- ifelse(ds225$sex == "male", 1, 0)
+ds225$region_num <- as.numeric(as.factor(ds225$region))
+
+# Select numeric columns only
+numeric_ds225 <- ds225[, c("age", "bmi", "children", "smoker_num", "sex_num", "region_num", "charges")]
+
+# Compute correlation matrix
+cor_matrix <- cor(numeric_ds225)
+
+# Display matrix
+cor_matrix
